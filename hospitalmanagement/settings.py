e363a120cf41e483377af4a220e65ee775064b43
mash-97 <mahimnhd97@gmail.com>
2022-05-18 16:57:22 +0600

    f

--- hospitalmanagement/settings.py
+++ hospitalmanagement/settings.py
@@ -132,7 +132,7 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 )
 
-MEDIA_ROOT=os.path.join(BASE_DIR,'static')
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL = 'media/'
 
 
